c50bbf448331c489416ebb1b66a5cfca44a46615
patw47 <patricia@wintrebert.ch>
2026-05-19 08:32:44 +0000

    modify vars

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
     script: 'n8n',
     args: 'start',
     env: {
-      N8N_PORT: 5680,
-      N8N_USER_FOLDER: '/home/acer/stock-tracker',
-      N8N_BASIC_AUTH_ACTIVE: 'true',
-      N8N_BASIC_AUTH_USER: 'admin',
-      N8N_BASIC_AUTH_PASSWORD: 'stockwatcher2026',
-      GENERIC_TIMEZONE: 'Europe/Paris',
-      N8N_DEFAULT_LOCALE: 'fr'
+      N8N_PORT: process.env.N8N_PORT,
+      N8N_USER_FOLDER: process.env.N8N_USER_FOLDER,
+      N8N_BASIC_AUTH_ACTIVE: process.env.N8N_BASIC_AUTH_ACTIVE,
+      N8N_BASIC_AUTH_USER: process.env.N8N_BASIC_AUTH_USER,
+      N8N_BASIC_AUTH_PASSWORD: process.env.N8N_BASIC_AUTH_PASSWORD,
+      GENERIC_TIMEZONE: process.env.GENERIC_TIMEZONE,
+      N8N_DEFAULT_LOCALE: process.env.N8N_DEFAULT_LOCALE
     }
   }]
 };
